2485526fbf64ccde1f4bd5539e04bcd6744af0c4
Obinna Ishmael Iloanya <81533820+Obilomania@users.noreply.github.com>
2022-03-27 09:02:50 +0800

    e-commerce-practice with React

---
+++ src/conponents/menuItem/Menu-Item.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './menus.scss'
+
+const MenuItem = ({ title, imageUrl, size}) => (
+    <div style={{
+        backgroundImage: `url(${imageUrl})`
+    } }
+        className={`${size} menu-item`}>
+        
+        <div className='background-image'
+            style=
+            {{
+                backgroundImage: `url(${imageUrl})`
+    }} />
+
+        <div className="content">
+            <h1 className="title">{title.toUpperCase()} </h1>
+            <span className="subtitle">SHOP NOW</span>
+        </div>
+    </div>
+);
+
+export default MenuItem;
---
+++ src/pages/Homepage/HomepageComponents.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './homepageStyle.scss';
+import Directory from '../../conponents/directory/directory';
+
+const HomepageComponents = () => {
+  return (
+      <div className='homepage'>
+          <Directory/>
+    </div>
+  )
+}
+
+export default HomepageComponents
